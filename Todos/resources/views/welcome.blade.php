1b582f4dea1b06c429d242749284a70d5bb625a4
hocdang1 <hoc.dang210210@vnuk.edu.vn>
2023-12-24 21:21:59 +0700

    fix code

--- Todos/resources/views/welcome.blade.php
+++ Todos/resources/views/welcome.blade.php
@@ -23,21 +23,19 @@
                 <th>Action</th>
             </tr>
             @foreach ($todos as $todo)
-
             <tr valign='middle'>
                 <td>{{$todo->name}}</td>
                 <td>{{$todo->work}}</td>
                 <td>{{$todo->dueDate}}</td>
                 <td>
-
-            @endforeach
-
-         
                     <a href="{{route("todo.edit",$todo->id)}}" class="btn btn-success btn-sm">Update</a>
                     <a href="{{route("todo.delete",$todo->id)}}" class="btn btn-danger btn-sm">Delete</a>
 
 
                 </td>
+            @endforeach
+
+            
             </tr>
         </table>
 
